031b9226e14f41bfc97f531a425f43a834623fa4
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-07-25 13:40:50 +0300

    ferst

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <main>
+      <article>
+        <section class="banner-main">
+          <div class="text-ban-wrp">
+            <div class="text-ban-back">
+              <h1>Змонтуємо вас безоплатно!</h1>
+              <h2>для Одеси та передмістя!</h2>
+            </div>
+          </div>  
+        </section>
+      </article>
+      <article class="text-bot-ban-wrp">
+          <p class="__text-main-inst">А Вы знали, что 80% случаев выхода из строя оборудования систем видеонаблюдения происходят по причине некачественного монтажа?</p>
+      </article>
+      <div class="line"></div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "scss/abstract.scss";
+@import "scss/main.scss";
+
+
+</style>
